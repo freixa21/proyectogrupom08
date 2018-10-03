53bc19469fefe0eb40628d7319c8646ab5483321
nekrats <36659680+nekrats@users.noreply.github.com>
2018-10-03 17:18:58 +0200

    Create Eric.java

---
+++ src/uf4p01/Eric.java
@@ -0,0 +1,16 @@
+public class Practica {
+
+    public static void main(String[] args) {
+
+    }
+public static void comprar (){
+    
+}
+public static void vender (){
+    
+}
+public static void salir(){
+    
+}
+
+}
