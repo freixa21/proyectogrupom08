bb2fc86a3f38a2fedbe3e8681877cf8e17457291
nekrats <36659680+nekrats@users.noreply.github.com>
2018-10-03 17:27:20 +0200

    modificació mètodes

--- src/uf4p01/Eric.java
+++ src/uf4p01/Eric.java
@@ -3,13 +3,13 @@ public class Tienda {
     public static void main(String[] args) {
 
     }
-public static void comprar (){
+public static void Eric (){
     
 }
-public static void vender (){
+public static void Luis (){
     
 }
-public static void salir(){
+public static void Max(){
     
 }
 
