a195b8275837d5632313eaf167a58c086845af05
AMX <AMX@AMX>
2018-10-03 16:52:05 +0200

    Linia esborrada.

--- src/uf4p01/proyectogrupom08.java
+++ src/uf4p01/proyectogrupom08.java
@@ -8,7 +8,7 @@ public class proyectogrupom08 {
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        metodo1();
+        
     }
     
     private static void metodo1() {
