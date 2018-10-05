d8c230194a99fe365b429af4c0426cefe954ebd3
nekrats <36659680+nekrats@users.noreply.github.com>
2018-10-05 17:55:01 +0200

    modificacion a la vez

--- src/uf4p01/Eric.java
+++ src/uf4p01/Eric.java
@@ -8,16 +8,12 @@ public static void Eric (){
     System.out.print ("Quiero crear conflicto :) ");
     
 }
-    public static void Eric (){
-    System.out.println("Aquest mètode es fals");
-    System.out.print ("Quiero crear conflicto :) ");
-    
-}
+
 public static void Luis (){
     System.out.print ("HOLAAA");
 }
 public static void Max(){
-    
+   system.out.print("hola"); 
 }
 
 }
